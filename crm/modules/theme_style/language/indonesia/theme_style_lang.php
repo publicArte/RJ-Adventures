a438dd78b317f29041c12b48cf4d7366f59ff91e
torhex137 <torhex137@gmail.com>
2020-03-17 01:16:52 -0600

    update

--- crm/modules/theme_style/language/indonesia/theme_style_lang.php
+++
@@ -1,4 +0,0 @@
-<?php
-#  Version 2.3.0
-$lang['theme_style']                            = 'Gaya Tema';
-
--- crm/modules/theme_style/language/spanish/theme_style_lang.php
+++
@@ -1,3 +0,0 @@
-<?php
-#  Version 2.3.0
-$lang['theme_style']                            = 'Estilo del tema';
